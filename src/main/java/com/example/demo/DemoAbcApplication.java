673c22f05ba68695dc622d8390d3de9b0a0f0a89
pachkawdepunam3 <pachkawdepunam3@gmail.com>
2023-01-13 16:32:28 +0530

    abc

---
+++ src/main/java/com/example/demo/DemoAbcApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoAbcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoAbcApplication.class, args);
+	}
+
+}
